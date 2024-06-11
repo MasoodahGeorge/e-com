90938727427cceac0f79755573be67b06cc30372
MasoodahGeorge <oodasbusiness@gmail.com>
2024-06-11 16:07:09 +0200

    ...

--- script/products.js
+++ script/products.js
@@ -23,6 +23,15 @@ let items = [item1, item2, item3, item4, item5, item6, item7, item8, item9];
 
 localStorage.setItem('items', JSON.stringify(items))
 
+// SPINNER
+if (!items || items.length === 0) {
+    document.querySelector('.spinner-border').style.display = 'block';
+} else {
+    document.querySelector('.spinner-border').style.display = 'none';
+    displayItems(items);
+}
+
+// Products
 let purchasedItems = [];
 
 function displayItems(items) {
@@ -64,10 +73,22 @@ document.getElementById('searchButton').addEventListener('click', () => {
 });
 
 //SPINNER
-window.onload = () => {
-    let spinner = document.querySelector('.spinner');
-    if (items.length === 0) {
-        spinner.style.display = 'block';}
+if (!items) {
+    items = [
+        new CreateItem(1, 'Floral Puffy Dress', 'Dresses', 'https://i.postimg.cc/fLDt366g/20240610-153941.jpg', 'Floral Puffy Dress Green Cream', 200.00, 1),
+        new CreateItem(2, 'Floral A-line Dress', 'Dresses', 'https://i.postimg.cc/5ytXg89B/20240610-153540.jpg', 'Floral A-line Dress Pink White', 250.00, 1),
+        new CreateItem(3, 'Silver Necklace', 'Accesories', 'https://i.postimg.cc/jdLWJ6FK/96189ee8aec7cb7bb07561283f784bb8-removebg-preview.png', 'Silver Necklace Purple Accesory', 60.00, 1),
+        new CreateItem(4, 'Silver Bracelet', 'Accesories', 'https://i.postimg.cc/wjN7wqzG/f07114f769fa71ea52f8ede85ef3a57f-removebg-preview.png', 'Silver Bracelet Purple Accesory', 50.00, 1),
+        new CreateItem(5, 'Pink Butterfly Heel', 'Shoes', 'https://i.postimg.cc/5tMjGhNN/20240610-153700.jpg', 'Pink High heel Butterfly', 250.00, 1),
+        new CreateItem(6, 'Purple Butterfly Heel', 'Shoes', 'https://i.postimg.cc/qM4TPp2F/20240610-153618-removebg-preview.png', 'Purple High heel Butterfly', 300.00, 1),
+        new CreateItem(7, 'Silver Bracelet', 'Accesories', 'https://i.postimg.cc/SxLmXJkk/40aaa04bd858d33712643a6ddc9206bc-removebg-preview.png', 'Silver Bracelet Pink Accesory', 50.00, 1),
+        new CreateItem(8, 'Floral Puffy Dress', 'Dresses', 'https://i.postimg.cc/gJjpBsR2/20240610-155942.jpg', 'Floral Puffy Dress Green Pink Cream', 200.00, 1),
+        new CreateItem(9, 'Pink Floral Heel', 'Shoes', 'https://i.postimg.cc/YSGK60wv/4b9dc57bba9283995265e1c0ef204b58-removebg-preview.png', 'Pink High heel Floral', 220.00, 1)
+    ];
+    localStorage.setItem('items', JSON.stringify(items));
+    displayItems(items);
+}
+
 //Cart
 function addToCart(id) {
     let item = items.filter(object => object.id == id) //items= the array above
@@ -79,4 +100,4 @@ purchasedButtons.forEach(button => {
     button.addEventListener('click', (event) => {
         addToCart(event.target.value)
     })
-})}
+})
