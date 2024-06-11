5f872e805295e51b34c52b48d8eb7c3e39f1fdf2
MasoodahGeorge <oodasbusiness@gmail.com>
2024-06-11 09:09:04 +0200

    .

--- script/products.js
+++ script/products.js
@@ -8,15 +8,15 @@ function CreateItem(id, name, category, image, description, price, quantity) {
     this.quantity = quantity
 }
 
-let item1 = new CreateItem(1, 'Long Dress', 'Clothing', 'https://i.postimg.cc/fLDt366g/20240610-153941.jpg', 'pink fit and flare dress', 200.00, 1)
-let item2 = new CreateItem(2, 'Short Dress', 'Clothing', 'https://i.postimg.cc/5ytXg89B/20240610-153540.jpg', 'pink off shoulder short dress', 250.00, 1)
+let item1 = new CreateItem(1, 'Long Dress', 'Dresses', 'https://i.postimg.cc/fLDt366g/20240610-153941.jpg', 'pink fit and flare dress', 200.00, 1)
+let item2 = new CreateItem(2, 'Short Dress', 'Dresses', 'https://i.postimg.cc/5ytXg89B/20240610-153540.jpg', 'pink off shoulder short dress', 250.00, 1)
 let item3 = new CreateItem(3, 'Hair Clip', 'Accesories', 'https://i.postimg.cc/jdLWJ6FK/96189ee8aec7cb7bb07561283f784bb8-removebg-preview.png', 'pink floral hair clip accesory', 60.00, 1)
 let item4 = new CreateItem(4, 'Scrunchie', 'Accesories', 'https://i.postimg.cc/wjN7wqzG/f07114f769fa71ea52f8ede85ef3a57f-removebg-preview.png', 'pink scrunchie hair tie', 20.00, 1)
-let item5 = new CreateItem(5, 'Perfume', 'Perfumes', 'https://i.postimg.cc/5tMjGhNN/20240610-153700.jpg', 'Amazing Pink Perfume', 300.00, 1)
-let item6 = new CreateItem(6, 'Perfume', 'Perfumes', 'https://i.postimg.cc/qM4TPp2F/20240610-153618-removebg-preview.png', 'Amazing Pink Perfume', 300.00, 1)
+let item5 = new CreateItem(5, 'Perfume', 'Shoes', 'https://i.postimg.cc/5tMjGhNN/20240610-153700.jpg', 'Amazing Pink Perfume', 300.00, 1)
+let item6 = new CreateItem(6, 'Perfume', 'Shoes', 'https://i.postimg.cc/qM4TPp2F/20240610-153618-removebg-preview.png', 'Amazing Pink Perfume', 300.00, 1)
 let item7 = new CreateItem(7, 'Hair Clip', 'Accesories', 'https://i.postimg.cc/SxLmXJkk/40aaa04bd858d33712643a6ddc9206bc-removebg-preview.png', 'pink floral hair clip accesory', 60.00, 1)
-let item8 = new CreateItem(8, 'Scrunchie', 'Clothing', 'https://i.postimg.cc/gJjpBsR2/20240610-155942.jpg', 'pink scrunchie hair tie', 20.00, 1)
-let item9 = new CreateItem(9, 'Perfume', 'Perfumes', 'https://i.postimg.cc/YSGK60wv/4b9dc57bba9283995265e1c0ef204b58-removebg-preview.png', 'Amazing Pink Perfume', 300.00, 1)
+let item8 = new CreateItem(8, 'Scrunchie', 'Dresses', 'https://i.postimg.cc/gJjpBsR2/20240610-155942.jpg', 'pink scrunchie hair tie', 20.00, 1)
+let item9 = new CreateItem(9, 'Perfume', 'Shoes', 'https://i.postimg.cc/YSGK60wv/4b9dc57bba9283995265e1c0ef204b58-removebg-preview.png', 'Amazing Pink Perfume', 300.00, 1)
 
 //display items
 let items = [item1, item2, item3, item4, item5, item6, item7, item8, item9];
